ab38759640b21a9acb05a4a79644ea1e4ee0716c
Nishad babu <122466935+nishad404@users.noreply.github.com>
2023-05-15 15:11:35 -0400

    Add files via upload

---
+++ string-method.js
@@ -0,0 +1,24 @@
+
+//slice method:
+let mama="apple,kola,mango,drink"; 
+console.log(mama.slice(5,10));
+console.log(mama.slice(-13,-7))
+
+//substring:
+let str = "Apple, Banana, Kiwi";
+console.log(str.substring(7,13));
+
+//substr:
+let s = "Apple, Banana, Kiwi";
+console.log(s.substr(7,6));
+
+//replace:
+let r="Please visit Microsoft!";
+console.log(r.replace("Microsoft","Hack By nishad"));
+
+//to-upper and lower:
+let ma="I am nishad";
+console.log(ma.toUpperCase());
+
+let gadha="I am nishad";
+console.log(gadha.toLowerCase());
