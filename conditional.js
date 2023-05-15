aae5ebf745ce95a365ce3eb4724873152096e9e3
Nishad babu <122466935+nishad404@users.noreply.github.com>
2023-05-15 16:15:47 -0400

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,30 @@
+
+    
+    //if-else statement:
+
+    if(5==10){
+        console.log(flase);
+    }
+    else if(1==1)
+    {
+        console.log(true);
+    };
+
+
+    //switch statement:
+
+     const eat=3;
+     switch(eat){
+        case 1:
+            console.log('fish');
+        break;
+        case 2:
+            console.log('burger');
+        break;
+        case 3:
+            console.log('chiken');
+        break;
+        default:
+            console.log("nothing");
+     } ; 
+
