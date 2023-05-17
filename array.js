9a970777c257d6a09d71bfef29dff329bcb673d2
Nishad babu <122466935+nishad404@users.noreply.github.com>
2023-05-17 15:00:40 -0400

    Add files via upload

---
+++ array.js
@@ -0,0 +1,23 @@
+//array in js::
+
+const nisha=["nishad","isa","hacker"];
+console.log(nisha[1]);
+console.log(nisha[nisha.length-1]);
+
+//loop in js::
+for(let i=0;i<nisha.length;i++){
+    console.log(nisha[i]);
+    console.log(i);
+}
+ function myfunction(a){
+       console.log(a);
+ }
+ nisha.forEach(myfunction);
+
+
+ let scores = [12, 55, 70];
+ function faruk(name,boy,girl){
+        console.log(name,boy,girl);
+ }
+
+ scores.forEach(faruk);
