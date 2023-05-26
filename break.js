b717eb4d2f4545c9858a423b15cdfcbfb451dd89
Nishad babu <122466935+nishad404@users.noreply.github.com>
2023-05-26 14:22:53 -0400

    Add files via upload

---
+++ break.js
@@ -0,0 +1,8 @@
+const animal=["dog","fog","elephant","mouse"];
+for(var i=0;i<animal.length;i++){
+  var animals=animal[i];
+    if(animals=="mouse"){
+        break;
+    }
+    console.log(animals);
+}
