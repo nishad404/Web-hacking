56d686233f08784ec7b642acc49e0fb9fa891c74
Nishad babu <122466935+nishad404@users.noreply.github.com>
2023-05-30 15:24:42 -0400

    Add files via upload

---
+++ map.js
@@ -0,0 +1,13 @@
+//map in js:::
+
+
+const map = new Map();
+map.set('banana', 1);
+map.set('orange', 2);
+map.set('meat', 4);
+
+//let obj = Object.fromEntries(map.entries()); 
+
+
+alert(obj.meat); 
+console.log(obj);
