f5135ff88d44ec1d1f26aa122dee5b98b52cd0f4
Nishad babu <122466935+nishad404@users.noreply.github.com>
2023-06-02 08:16:45 -0400

    Add files via upload

---
+++ normal_arrow_function.js
@@ -0,0 +1,16 @@
+
+//normal function in js:::
+
+function add(first,second){
+    const sum=first+second;
+    return sum;
+}
+const name=add(10,20);
+console.log(name);
+
+
+//arrow function in js::::
+
+const value=(a,b) =>a+b;
+const total=value(10,30);
+console.log(total);
