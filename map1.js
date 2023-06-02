ade93b3e7fefa760e9d851d3c61e687d1208580c
Nishad babu <122466935+nishad404@users.noreply.github.com>
2023-06-02 14:22:49 -0400

    Add files via upload

---
+++ map1.js
@@ -0,0 +1,12 @@
+
+
+const makedouble=[1,2,3,4,5].map(x=>x*5);
+console.log(makedouble);
+
+
+const number=[65,13,20,40];
+function myfunction(num){
+    return num*5;
+}
+const value=number.map(myfunction);
+console.log(value);
