9c3ddf1e41e96b76dc450122ad2202185340d018
Nishad babu <122466935+nishad404@users.noreply.github.com>
2023-06-08 13:43:59 -0400

    Add files via upload

---
+++ ayschronous.js
@@ -0,0 +1,11 @@
+const process = (customer) => {
+    console.log('process order for customer1');
+    setTimeout(() => {
+        console.log('cooking complete');
+    },3000);
+
+    console.log('order process for customer 1');
+    };
+    console.log('take order for customer 1');
+    process();
+    console.log('complete order for customer 1');
