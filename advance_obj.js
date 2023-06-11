6020657ea33b07e6ab90e315b99b1e789b2bd0ea
Nishad babu <122466935+nishad404@users.noreply.github.com>
2023-06-11 14:21:48 -0400

    Add files via upload

---
+++ advance_obj.js
@@ -0,0 +1,27 @@
+
+function person(firstname,lastname){
+    return{
+        firstname:firstname,
+        lastname:lastname,
+        fullname:function(){
+            console.log(this.firstname + this.lastname);
+        }
+
+    }
+
+}
+console.log(person('nishad','babu')); 
+
+
+function Person1(firstname1,lastname2){
+   
+        this.firstname1=firstname1;
+        this.lastname=lastname2;
+        this.fullname=function(){
+            console.log(this.firstname1 + this.lastname2);
+        };
+
+    }
+
+    const per=new Person1('ami','tumi');
+    console.log(per);
