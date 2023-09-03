92f07ce11707b1631fb5dfdab3ce99c701c89f91
Nishad babu <122466935+nishad404@users.noreply.github.com>
2023-09-03 19:19:52 +0600

    Add files via upload

---
+++ input.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+const myname=parseInt(prompt("enter first: a"));
+const myname1=parseInt(prompt("enter first: b"));
+const name1=myname+myname1;
+console.log(`this is ${myname} and ${myname1} is ${name1}`);
+alert(myname2);
+
+
+
+////
+var a = parseInt(prompt("Enter first number"));
+var b = parseInt(prompt("Enter second number"));
+
+console.log(a + b);
