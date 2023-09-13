683fa261cdf1184327c9e70a3dbe53c2f4a0caf6
Nishad babu <122466935+nishad404@users.noreply.github.com>
2023-09-13 21:08:06 +0600

    Add files via upload

---
+++ call_back.js
@@ -0,0 +1,15 @@
+function getmyname(num1,num2,nishad){
+         if(num1==30){
+              nishad();
+         }
+         else{
+              alert("sorry,please try again !");
+         }
+}
+
+function getmyvalue(){
+       alert("i am cyber king !!");
+}
+let num1=30;
+let num2=40;
+getmyname(num1,num2,getmyvalue);
